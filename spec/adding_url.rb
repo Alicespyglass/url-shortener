24d1dbe3f11c54777a85ca3bdb340accfb7bbf58
alicespyglass <alicespyglass@gmail.com>
2017-06-27 01:47:29 +0100

    write feature test for adding url

---
+++ spec/adding_url.rb
@@ -0,0 +1,14 @@
+feature 'Adding URL' do
+
+  scenario 'I can add a new url' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.beans.com/'
+    click_button 'Shorten url'
+
+    expect(current_path).to eq '/links'
+
+    within 'ul#links' do
+      expect(page).to have_content('http://www.beans.com/')
+    end
+  end
+end
